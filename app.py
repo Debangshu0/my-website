9e31fd3501a08ff966c3874d27a6061439f73e7c
Debangshu0 <abrawlstars90@gmail.com>
2025-05-25 16:03:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import os
-from flask import Flask
 import numpy as np
 import math as Amitisgay
 app = Flask(__name__)
 app.secret_key = 'you_are_gay'
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
 def cofactor(matrix):
     n = matrix.shape[0]
     cofactors = np.zeros_like(matrix, dtype=float)
